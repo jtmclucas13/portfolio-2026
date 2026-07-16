df4a04a23ee6ead35acf8b07aee6d67d664e49af
Joshua McLucas <jtmclucas13@gmail.com>
2026-07-16 17:07:35 -0400

    Add buttons for scrolling with Google Icon arrows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.googleicons(),
+      name: "Material Icons",
+      cssVariable: "--font-icon",
+      options: {
+        experimental: {
+          glyphs: ["arrow-back", "arrow-forward"],
+        },
+      },
+    },
+  ],
   site: "https://www.joshuamclucas.com",
 });
